cba608e92b3dec7d0b4da355d1c2a0a3de4f0184
pxfnc <dev.pxfnc@gmail.com>
2020-02-24 09:40:55 +0900

    レンダリング復帰時の位置がおかしくなるのを修正

--- dvd.js
+++ dvd.js
@@ -3,16 +3,18 @@ const dvdFieldDOM = document.getElementById("dvd-field");
 class DVD {
 
   constructor(dvdSvgDOM) {
-    const acc = 0.12
 
     this.dom = dvdSvgDOM
     this.coord = { x: 0, y: 0 }
-    this.speed = { x: acc, y: acc }
+    this.speed = 0.26
     this.rgbVec3 = [0, 0, 0]
     this.previousTime = 0
 
     this.dom.onclick = () => this.changeColor()
     this.dom.ontouchstart = () => this.changeColor()
+
+    this.changeColor()
+
   }
 
   update(t) {
@@ -22,24 +24,17 @@ class DVD {
     const areaWidth = dvdFieldDOM.clientWidth - this.dom.clientWidth
     const areaHeight = dvdFieldDOM.clientHeight - this.dom.clientHeight
 
-    let needColorChange = false
+    const x = (this.coord.x + this.speed * dt) % (areaWidth * 2)
+    const y = (this.coord.y + this.speed * dt) % (areaHeight * 2)
 
-    if (this.coord.x < 0 && this.speed.x < 0 || this.coord.x > areaWidth && this.speed.x > 0) {
-      this.speed.x = this.speed.x * -1
-      needColorChange = true
-    }
-    if (this.coord.y < 0 && this.speed.y < 0 || this.coord.y > areaHeight && this.speed.y > 0) {
-      this.speed.y = this.speed.y * -1
-      needColorChange = true
-    }
+    if ((Math.floor(this.coord.x / areaWidth) != Math.floor(x / areaWidth))
+      || (Math.floor(this.coord.y / areaHeight) != Math.floor(y / areaHeight)))
+      this.changeColor()
 
-    if (needColorChange) this.changeColor()
+    this.coord = {x, y}
 
-    this.coord.x = this.coord.x + this.speed.x * dt
-    this.coord.y = this.coord.y + this.speed.y * dt
-
-    this.dom.style.left = this.coord.x + "px";
-    this.dom.style.top = this.coord.y + "px";
+    this.dom.style.left = areaWidth - Math.abs(this.coord.x - areaWidth) + "px";
+    this.dom.style.top = areaHeight - Math.abs(this.coord.y - areaHeight) + "px";
 
     this.previousTime = t
 
